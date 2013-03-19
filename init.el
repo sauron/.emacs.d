7b73ff5d8ac30933651557a45600835aee8cba6c
Pablo Barrios <ingpablobarrios@gmail.com>
2013-03-19 16:47:02 -0300

    Added minimal customization

---
+++ init.el
@@ -0,0 +1,12 @@
+(setq pgb-emacs-init-file load-file-name)
+(setq pgb-emacs-config-dir
+      (file-name-directory pgb-emacs-init-file))
+ 
+;; Set up 'custom' system
+(setq custom-file (expand-file-name "emacs-customizations.el" pgb-emacs-config-dir))
+(load custom-file)
+
+(setq user-emacs-directory pgb-emacs-config-dir)
+ 
+(setq backup-directory-alist
+      (list (cons "." (expand-file-name "backup" user-emacs-directory))))
