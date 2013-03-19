2be9914c5703ca73ddf8d89884fce25789362f04
Pablo Barrios <ingpablobarrios@gmail.com>
2013-03-19 17:03:58 -0300

    Minor Refactor

--- init.el
+++ init.el
@@ -1,12 +1,13 @@
+;; Files and directories
+
 (setq pgb-emacs-init-file load-file-name)
 (setq pgb-emacs-config-dir
       (file-name-directory pgb-emacs-init-file))
- 
+(setq user-emacs-directory pgb-emacs-config-dir)
+(setq backup-directory-alist
+      (list (cons "." (expand-file-name "backup" user-emacs-directory)))) 
+
 ;; Set up 'custom' system
 (setq custom-file (expand-file-name "emacs-customizations.el" pgb-emacs-config-dir))
 (load custom-file)
 
-(setq user-emacs-directory pgb-emacs-config-dir)
- 
-(setq backup-directory-alist
-      (list (cons "." (expand-file-name "backup" user-emacs-directory))))
