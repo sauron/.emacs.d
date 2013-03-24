28396966096fbc5e68d71c92f2b44ab85ed73444
Pablo Barrios <ingpablobarrios@gmail.com>
2013-03-24 01:15:25 -0300

    Added IDo mode, also enabled gists

--- init.el
+++ init.el
@@ -6,8 +6,23 @@
 (setq user-emacs-directory pgb-emacs-config-dir)
 (setq backup-directory-alist
       (list (cons "." (expand-file-name "backup" user-emacs-directory)))) 
+(setq pgb-elisp-dir (expand-file-name "elisp" pgb-emacs-config-dir))
+(setq pgb-elisp-external-dir
+      (expand-file-name "external" pgb-elisp-dir))
+(setq pgb-secrets-file (expand-file-name "secrets.el" pgb-emacs-config-dir))
 
 ;; Set up 'custom' system
 (setq custom-file (expand-file-name "emacs-customizations.el" pgb-emacs-config-dir))
 (load custom-file)
 
+; Add external projects to load path
+(dolist (project (directory-files pgb-elisp-external-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
+
+(require 'yagist)
+
+(setq gist-authenticate-function 'gist-basic-authentication)
+
+(when (file-exists-p pgb-secrets-file)
+  (load pgb-secrets-file))
