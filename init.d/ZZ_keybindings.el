da222b066b39ad91159408993763c5772c6f7bfd
Pablo Barrios <ingpablobarrios@gmail.com>
2013-04-10 20:39:22 -0300

    Added another key support

--- init.d/ZZ_keybindings.el
+++ init.d/ZZ_keybindings.el
@@ -1,3 +1,4 @@
 (global-set-key (kbd "M-ñ") '"~") ; Meta+ñ => ~
 (global-set-key (kbd "M-2") '"@") ; Meta+2 => @
 (global-set-key (kbd "M-+") '"]") ; Meta+2 => ]
+(global-set-key (kbd "M-ç") '"}") ; Meta+ç => }
